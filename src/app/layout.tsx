b867bf100f5af784522d4df937e41b8719dbdbc6
Pablo <pblpawelmilczak@gmail.com>
2023-08-31 15:49:29 +0200

    feat: Add toast functionality

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import Link from 'next/link';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -15,6 +17,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en">
       <body className={inter.className}>
         <div className="w-full flex flex-col items-center p-4 ">
+          <ToastContainer />
           <div className="flex flex-col gap-6 min-w-[240px] max-w-[540px] w-full">
             <header className="flex w-full gap-4">
               <Link href="/" className="bg-actionSecondary py-2 px-6 rounded-md font-semibold">
--- src/utils/services.ts
+++ src/utils/services.ts
@@ -1,5 +1,14 @@
 import { ApodPhotoData } from '@/app/page';
+import { toast } from 'react-toastify';
 
-export const savePhoto = (photoData: ApodPhotoData) => {
-  fetch('http://localhost:3000/api', { method: 'POST', body: JSON.stringify(photoData) });
+export const savePhoto = async (photoData: ApodPhotoData) => {
+  const res = await fetch('http://localhost:3000/api', {
+    method: 'POST',
+    body: JSON.stringify(photoData),
+  });
+
+  if (res.status === 200) return toast('Photo saved', { type: 'success' });
+
+  const toastMessage = await res.text();
+  toast(toastMessage, { type: 'error' });
 };
